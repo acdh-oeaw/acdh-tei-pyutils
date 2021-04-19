c139cc1bf5b1dbe0a173a7c0d1693c935df24463
csae8092 <peter.andorfer@oeaw.ac.at>
2021-04-19 11:22:12 +0200

    Bump version: 0.10.0 → 0.11.0

--- setup.py
+++ setup.py
@@ -74,6 +74,6 @@ setup(
     test_suite='tests',
     tests_require=test_requirements,
     url='https://github.com/csae8092/acdh-tei-pyutils',
-    version='0.10.0',
+    version='0.11.0',
     zip_safe=False,
 )
