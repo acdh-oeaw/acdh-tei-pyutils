5b9e5901ee2ce3ef7818924672c34278e0d564e9
Daniel_Stoxreiter <daniel.stoxreiter@outlook.de>
2022-11-29 13:08:20 +0100

    removed test files; updated version to 0.27.0

--- denormalize.sh
+++
@@ -1,3 +0,0 @@
-# bin/bash
-
-denormalize-indices -f "/home/daniel/Documents/Auden-Musulin/amp-app-dev/data/editions/*/*.xml" -i "/home/daniel/Documents/Auden-Musulin/amp-app-dev/data/indices/*.xml" -m './/tei:rs[@ref]/@ref' -t 'mentioned in ' -x './/tei:title[@level="a"]/text()'
