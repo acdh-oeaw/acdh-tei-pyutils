767f2efce89d62c9d15d8fdf176751d6bae40952
csae8092 <peter.andorfer@oeaw.ac.at>
2023-01-19 11:59:33 +0100

    formatting, version bump; updated pypi wf

--- setup.py
+++ setup.py
@@ -23,59 +23,60 @@ def walker(base, *paths):
     return list(file_list)
 
 
-with open('README.rst') as readme_file:
+with open("README.rst") as readme_file:
     readme = readme_file.read()
 
-with open('CHANGELOG.rst') as history_file:
+with open("CHANGELOG.rst") as history_file:
     history = history_file.read()
 
-with open('requirements.txt') as f:
+with open("requirements.txt") as f:
     requirements = f.read().splitlines()
 
-setup_requirements = ['pytest-runner', ]
+setup_requirements = [
+    "pytest-runner",
+]
 
-test_requirements = ['pytest>=3', ]
+test_requirements = [
+    "pytest>=3",
+]
 
 setup(
     author="Peter Andorfer",
-    author_email='peter.andorfer@oeaw.ac.at',
-    python_requires='>=3.7',
+    author_email="peter.andorfer@oeaw.ac.at",
+    python_requires=">=3.7",
     classifiers=[
-        'Development Status :: 2 - Pre-Alpha',
-        'Intended Audience :: Developers',
-        'License :: OSI Approved :: MIT License',
-        'Natural Language :: English',
-        'Programming Language :: Python :: 3',
-        'Programming Language :: Python :: 3.7',
-        'Programming Language :: Python :: 3.8',
+        "Development Status :: 2 - Pre-Alpha",
+        "Intended Audience :: Developers",
+        "License :: OSI Approved :: MIT License",
+        "Natural Language :: English",
+        "Programming Language :: Python :: 3",
+        "Programming Language :: Python :: 3.7",
+        "Programming Language :: Python :: 3.8",
     ],
     description="Utilty functions to work with TEI Documents",
     entry_points={
-        'console_scripts': [
-            'add-attributes=acdh_tei_pyutils.cli:add_base_id_next_prev',
-            'mentions-to-indices=acdh_tei_pyutils.cli:mentions_to_indices',
-            'denormalize-indices=acdh_tei_pyutils.cli:denormalize_indices',
-            'add-handles=acdh_tei_pyutils.cli:add_handles',
-            'schnitzler=acdh_tei_pyutils.cli:schnitzler'
+        "console_scripts": [
+            "add-attributes=acdh_tei_pyutils.cli:add_base_id_next_prev",
+            "mentions-to-indices=acdh_tei_pyutils.cli:mentions_to_indices",
+            "denormalize-indices=acdh_tei_pyutils.cli:denormalize_indices",
+            "add-handles=acdh_tei_pyutils.cli:add_handles",
+            "schnitzler=acdh_tei_pyutils.cli:schnitzler",
         ],
     },
     install_requires=requirements,
     license="MIT",
-    long_description=readme + '\n\n' + history,
+    long_description=readme + "\n\n" + history,
     include_package_data=True,
-    keywords='acdh-tei-pyutils',
-    name='acdh-tei-pyutils',
-    packages=find_packages(include=['acdh_tei_pyutils', 'acdh_tei_pyutils.*']),
+    keywords="acdh-tei-pyutils",
+    name="acdh-tei-pyutils",
+    packages=find_packages(include=["acdh_tei_pyutils", "acdh_tei_pyutils.*"]),
     package_data={
-        module.__name__: walker(
-            os.path.dirname(module.__file__),
-            'files'
-        ),
+        module.__name__: walker(os.path.dirname(module.__file__), "files"),
     },
     setup_requires=setup_requirements,
-    test_suite='tests',
+    test_suite="tests",
     tests_require=test_requirements,
-    url='https://github.com/csae8092/acdh-tei-pyutils',
-    version='0.27.0',
+    url="https://github.com/csae8092/acdh-tei-pyutils",
+    version="v0.28",
     zip_safe=False,
 )
