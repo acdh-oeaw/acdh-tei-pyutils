5b9d8165997df48d7b893270a3c7cc4b4c9ab6fc
csae8092 <peter.andorfer@oeaw.ac.at>
2023-01-23 13:08:33 +0100

    version bump

--- setup.py
+++ setup.py
@@ -70,6 +70,6 @@ setup(
     test_suite="tests",
     tests_require=dev_requirements,
     url="https://github.com/acdh-oeaw/acdh-tei-pyutils",
-    version="v0.28",
+    version="v0.29",
     zip_safe=False,
 )
