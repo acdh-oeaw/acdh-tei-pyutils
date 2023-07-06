f1ec4221f249b74f67928e9ad1623b24d500c940
Peter Andorfer <peter.andorfer@oeaw.ac.at>
2023-07-06 14:14:59 +0200

    now correct version bump [skip ci]

--- setup.py
+++ setup.py
@@ -70,6 +70,6 @@ setup(
     test_suite="tests",
     tests_require=dev_requirements,
     url="https://github.com/acdh-oeaw/acdh-tei-pyutils",
-    version="v0.4",
+    version="v0.32",
     zip_safe=False,
 )
