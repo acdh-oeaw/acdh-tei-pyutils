8062bd02195f62a71f449d164ae7bace3a37abd0
csae8092 <p.andorfer@gmail.com>
2024-11-05 09:40:25 +0100

    version bump [skip ci]

--- setup.py
+++ setup.py
@@ -70,6 +70,6 @@ setup(
     test_suite="tests",
     tests_require=dev_requirements,
     url="https://github.com/acdh-oeaw/acdh-tei-pyutils",
-    version="v1.5",
+    version="v1.6",
     zip_safe=False,
 )
